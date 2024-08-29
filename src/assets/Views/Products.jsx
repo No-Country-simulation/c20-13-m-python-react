7e5cffa4caf5f9a0f1ef6661a7a7306e27e526af
Adriana Cabrera Escobar <adricanita_c@hotmail.com>
2024-08-29 20:40:53 +0000

    Create carousel of products

--- src/assets/Views/Products.jsx
+++ src/assets/Views/Products.jsx
@@ -2,24 +2,37 @@ import React from 'react'
 import '../styles/Products.css'
 import logo from "/src/assets/img/logo.png";
 import { Cards } from '../components/Cards.jsx'
+import { Carousel } from 'react-bootstrap';
 
 export const Products = () => {
-  
-  const products = Array.from({length: 10}, (_, index) => ({
+
+  const products = Array.from({ length: 10 }, (_, index) => ({
     id: index + 1,
     image: logo,
     text: `Producto ${index + 1}`,
   }))
+
+  const productGroups = []
+  for (let i = 0; i < products.length; i += 2) {
+    productGroups.push(products.slice(i, i + 2))
+  }
+
   return (
-    <div className="container justify-content-center">
-      <div className="row ">
-        {products.map((product) => ( 
-          <div className="col-md-6 mb-4 " key={product.id}>
-          <Cards image={product.image} altText={product.text}/>
-        </div>
-          ))}
-      </div>
+    <div className="container">
+      <Carousel>
+      {productGroups.map((group, index) => (
+          <Carousel.Item key={index}>
+            <div className="row">
+              {group.map((product) => (
+                <div className="col-md-6 mb-4" key={product.id}>
+                  <Cards image={product.image} text={product.text} />
+                </div>
+              ))}
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/Cards.css'
 
-export const Cards = ({image, text}) => {
+export const Cards = ({ image, text }) => {
 
     return (
         <div className="card">
             <img src={image} className="card-img-top" alt={text}></img>
+            <h3 className="card-title">Nombre Producto</h3>
+            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui, repellendus aspernatur amet in aut atque suscipit veniam minima eum consequuntur vero nesciunt porro earum ex nulla accusamus itaque illo.</p>
             <div className="card-body d-flex justify-content-center">
                 <button className="btn btn-secondary">
                     Comprar
