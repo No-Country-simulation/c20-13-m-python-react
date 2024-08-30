ed74110fe38bb2675cb6bb2c47d33a4901cf7d11
Trinidad Sabina Lari <trinidadlari@gmail.com>
2024-08-30 10:03:55 -0300

    "marge done"

--- src/assets/Views/Products.jsx
+++ src/assets/Views/Products.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import '../styles/Products.css'
 import logo from "/src/assets/img/logo.png";
 import { Cards } from '../components/Cards.jsx'
+import Footer from '../components/Footer.jsx';
 
 export const Products = () => {
-  
-  const products = Array.from({length: 10}, (_, index) => ({
+
+  const products = Array.from({ length: 10 }, (_, index) => ({
     id: index + 1,
     image: logo,
     text: `Producto ${index + 1}`,
@@ -13,12 +14,13 @@ export const Products = () => {
   return (
     <div className="container justify-content-center">
       <div className="row ">
-        {products.map((product) => ( 
+        {products.map((product) => (
           <div className="col-md-6 mb-4 " key={product.id}>
-          <Cards image={product.image} altText={product.text}/>
-        </div>
-          ))}
+            <Cards image={product.image} altText={product.text} />
+          </div>
+        ))}
       </div>
+      <Footer />
     </div>
   )
 }
