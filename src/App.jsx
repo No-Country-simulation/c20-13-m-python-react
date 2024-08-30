429a822e867a3b08d954f3ed2be9423762646980
Trinidad Sabina Lari <trinidadlari@gmail.com>
2024-08-30 09:51:01 -0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './App.css';
 
 function App() {
 
-  return
+  return (
+    <>
+      <Home />
+      <Products />
+    </>
+  )
 }
 
 export default App
