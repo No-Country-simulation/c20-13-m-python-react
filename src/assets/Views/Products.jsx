35300fa2ee01ef132c4dd1358d5af40aa5b62b5a
Adriana Cabrera Escobar <adricanita_c@hotmail.com>
2024-08-30 13:12:08 +0000

    Change card text and image size

--- src/assets/Views/Products.jsx
+++ src/assets/Views/Products.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import '../styles/Products.css'
+import '../styles/products.css'
 import logo from "/src/assets/img/logo.png";
 import { Cards } from '../components/Cards.jsx'
-import { Carousel } from 'react-bootstrap';
 
 export const Products = () => {
 
@@ -12,26 +11,15 @@ export const Products = () => {
     text: `Producto ${index + 1}`,
   }))
 
-  const productGroups = []
-  for (let i = 0; i < products.length; i += 2) {
-    productGroups.push(products.slice(i, i + 2))
-  }
-
   return (
-    <div className="container">
-      <Carousel>
-      {productGroups.map((group, index) => (
-          <Carousel.Item key={index}>
-            <div className="row">
-              {group.map((product) => (
-                <div className="col-md-6 mb-4" key={product.id}>
-                  <Cards image={product.image} text={product.text} />
-                </div>
-              ))}
-            </div>
-          </Carousel.Item>
+    <div className="container-product">
+      <div className="row">
+        {products.map((product) => (
+          <div className="col-sm-6 mb-4" key={product.id}>
+            <Cards image={product.image} text={product.text} />
+          </div>
         ))}
-      </Carousel>
+      </div>
     </div>
   );
 };
