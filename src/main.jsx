a4876ea1f83a6083417b5cbc558bd994f1f39daf
Miguel Angel Gonzales <mitronios@gmail.com>
2024-08-30 08:46:41 -0400

    Adds not found

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,23 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./assets/Views/Home.jsx";
 import { Products } from "./assets/Views/Products.jsx";
+import { NotFound } from "./assets/Views/NotFound.jsx";
+
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Home />
   },
   {
     path: "/products",
     element: <Products/>
+  },
+  {
+    path: "/*",
+    element: <NotFound/>
   }
 ]);
 createRoot(document.getElementById("root")).render(
