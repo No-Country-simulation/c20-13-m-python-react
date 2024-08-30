898aef159b63a78fd41754a42bb5eebb53889778
Fiorealbujar <fiorerut8@hotmail.com>
2024-08-30 11:55:28 -0300

    navbar, cart, sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Home from './assets/components/Home';
 import { Products } from "./assets/Views/Products";
+import Navbar from './assets/components/Navbar';
 
 function App() {
 
   return (
     <>
+      <Navbar/>
       <Home/>
       <Products/>
     </>
