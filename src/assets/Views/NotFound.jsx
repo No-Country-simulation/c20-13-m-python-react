e1a4cf1e34530f6de9b1755f99e30312c374755a
TrinidadLari <141693507+TrinidadLari@users.noreply.github.com>
2024-09-02 12:31:38 -0300

    Revert "Trinidad"

--- src/assets/Views/NotFound.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-
-export const NotFound = () => {
-  return <h1>Not Found</h1>}
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./assets/Views/Home.jsx";
-import { Products } from "./assets/Views/Products.jsx";
-import { NotFound } from "./assets/Views/NotFound.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/products",
-    element: <Products/>
-  },
-  {
-    path: "/*",
-    element: <NotFound/>
-  }
-]);
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
