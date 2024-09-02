036c704693f1bb499945dc61fa94d22690a78889
Fiorealbujar <fiorerut8@hotmail.com>
2024-09-02 11:27:46 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import './App.css';
 import Home from './assets/components/Home';
 import { Products } from "./assets/Views/Products";
 import Navbar from './assets/components/Navbar';
+import Sidebar from './assets/components/Sidebar';
+import Cart from './assets/components/Cart';
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <div className="main-content">
+        <Sidebar />
+        <ProductList />
+        <Cart />
+      </div>
       <Home/>
       <Products/>
     </>
