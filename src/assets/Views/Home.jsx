1c2a2137f2e0cbcf32a2a7954f1fc14fc1b6996b
Trinidad Sabina Lari <trinidadlari@gmail.com>
2024-09-04 10:24:40 -0300

    Changes styles

--- src/assets/Views/Home.jsx
+++ src/assets/Views/Home.jsx
@@ -7,20 +7,20 @@ import { Pay } from "./Pay";
 const Home = () => {
   return (
     <>
-        <header className="container">
-          <h1>BerryChill</h1>
-          <h2>Los mejores frutos congelados a un click de distancia</h2>
-        </header>
-        <section className="container">
-          <img className="img-fluid" src={logo} alt="Berrychill Logo" />
-        </section>
-        <div className="bt d-grid col-xm-6 col-md-4 col-xl-2 mx-auto">
-          <Link to={"/products"}>
-            <button type="button" className="btn btn-secondary btn-lg">
-              Comprar
-            </button>
-          </Link>
-        </div>
+      <header className="container">
+        <h1>BerryChill</h1>
+        <h2>Los mejores frutos congelados a un click de distancia</h2>
+      </header>
+      <section className="container">
+        <img className="img-fluid" src={logo} alt="Berrychill Logo" />
+      </section>
+      <div className="bt d-grid col-xm-6 col-md-4 col-xl-2 mx-auto">
+        <Link to={"/products"}>
+          <button type="button" className="btn btn-secondary btn-lg">
+            Comprar
+          </button>
+        </Link>
+      </div>
     </>
   );
 };
