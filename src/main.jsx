cad50231ad96984c10f0dd411fa6d7a6bda7ca05
Miguel Angel Gonzales <mitronios@gmail.com>
2024-09-05 09:22:16 -0400

    Adds checkout and pay routes, modifies index.css

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,10 @@ import Home from "./assets/Views/Home.jsx";
 import { Products } from "./assets/Views/Products.jsx";
 import { NotFound } from "./assets/Views/NotFound.jsx";
 import AboutUs from "./assets/Views/AboutUs.jsx";
+import { Pay } from "./assets/Views/Pay.jsx";
+import { Checkout } from "./assets/Views/CheckOut.jsx";
 import "./index.css";
-import "bootstrap/dist/css";
+import "bootstrap";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,14 @@ const router = createBrowserRouter([
   {
     path: "/aboutus",
     element: <AboutUs />
+  },
+  {
+    path:"/pay",
+    element: <Pay/>
+  },
+  {
+    path:"/checkout",
+    element: <Checkout/>
   },
   {
     path: "/*",
