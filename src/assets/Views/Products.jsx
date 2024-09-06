184c25775c3c5a4ad162129fa630bf3701e05749
Adriana Cabrera Escobar <adricanita_c@hotmail.com>
2024-09-06 10:47:39 -0400

    Revert "Pull request from Adriana"

--- src/assets/Views/Products.jsx
+++ src/assets/Views/Products.jsx
@@ -1,11 +1,9 @@
-
-import React from "react"
-import "../styles/products.css"
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import '../styles/Products.css'
 import logo from "/src/assets/img/logo.png";
-import { Cards } from "../components/Cards.jsx"
-import { Carousel } from "../components/Carousel.jsx"
+import { Cards } from '../components/Cards.jsx'
 import Footer from '../components/Footer.jsx';
-
 
 export const Products = () => {
 
@@ -14,22 +12,17 @@ export const Products = () => {
     image: logo,
     text: `Producto ${index + 1}`,
   }))
-
   return (
-    <React.Fragment>
-      <Carousel />
-      <div className="container-product">
-        <div className="row">
-          {products.map((product) => (
-            <div className="col-sm-6 mb-4" key={product.id}>
-              <Cards image={product.image} text={product.text} />
-            </div>
-          ))}
-        </div>
-        <Footer />
+    <div className="container justify-content-center">
+      <div className="row ">
+        {products.map((product) => (
+          <div className="col-md-6 mb-4 " key={product.id}>
+            <Cards image={product.image} altText={product.text} />
+          </div>
+        ))}
       </div>
-    </React.Fragment>
-  );
-};
-
+      <Footer />
+    </div>
+  )
+}
 
