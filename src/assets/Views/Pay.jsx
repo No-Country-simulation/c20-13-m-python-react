7ac43869c8498fa69d6f070da97bc4161fc2f2af
Miguel Angel Gonzales <mitronios@gmail.com>
2024-09-19 09:19:26 -0400

    Feat: Add checkout and pay

--- src/assets/Views/Pay.jsx
+++ src/assets/Views/Pay.jsx
@@ -1,18 +1,56 @@
 import React from "react";
 import Footer from "../components/Footer";
 import "../styles/pay.css";
+import { SiMercadopago } from "react-icons/si";
+import { ImgCarousel } from "../components/ImgCarousel";
 
 export const Pay = () => {
   return (
     <>
+      <ImgCarousel />
       <div className="pay">
         <h1>Elegí como pagar</h1>
-        <h3>Pagás: $x</h3>
-        <button type="button" className="btn btn-secondary btn-sm">
-          Ir a Mercado Pago
-        </button>
+        <div
+          className="dropdown d-flex align-items-center mt-4"
+          style={{ gap: "21px", border: "solid 1px" }}
+        >
+          <h3>Transferencia</h3>
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton2"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <SiMercadopago size={52} />
+            Mercado Pago
+          </button>
+          <ul
+            className="dropdown-menu dropdown-menu-dark"
+            aria-labelledby="dropdownMenuButton2"
+          >
+            <li>
+              <a className="dropdown-item active" href="#">
+                Action
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <Footer />
+      <div className="container card text-center" style={{ maxWidth: "25rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">Pago contra entrega</h5>
+          <p className="card-text">
+            Coordina el pago y la entrega con el vendedor
+          </p>
+          <a href="#" className="btn btn-primary" style={{ width: "18rem" }}>
+            Realizar pedido
+          </a>
+        </div>
+      </div>
+      <div className="fixed-bottom">
+        <Footer />
+      </div>
     </>
   );
 };
